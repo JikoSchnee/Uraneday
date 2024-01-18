9555f0d49b43f502e4bd5c428db89f59fc9de7b8
JikoSchnee <104906844+JikoSchnee@users.noreply.github.com>
2024-01-18 17:17:34 +0800

    Components: floating card
    新加入了一个带动画的悬停组件

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
 <!--    <CardElementPlus></CardElementPlus>-->
     <CardPlain></CardPlain>
+    <product-card></product-card>
 </template>
 
 
 <script>
 // import CardElementPlus from "@/components/ForAllComponents/CardElementPlus.vue";
-import CardPlain from "@/components/ForAllComponents/CardPlain.vue";
+import CardPlain from "@/components/ForAllComponents/Card/CardPlain.vue";
+import ProductCard from "@/components/ForAllComponents/ProductCard.vue";
 
 export default {
     components: {
+        ProductCard,
         // CardElementPlus,
         CardPlain
     },
